60ae632da3585a7f7b2f0e2855074b045eaef17e
Upendra Gusain <upendra.gusain@gmail.com>
2020-04-16 16:00:58 +0100

    add candeactivate create event

--- src/app/events/create-event.component.ts
+++ src/app/events/create-event.component.ts
@@ -7,6 +7,7 @@ import { Router } from '@angular/router'
 })
 
 export class CreateEventComponent {
+  isDirty:boolean = true;
   constructor(private router: Router) {
   }
 
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -6,7 +6,10 @@ import { Error404Component } from './errors/404.component'
 import { EventRouteActivator } from './events/event-details/event-route-activator.component'
 
 export const appRoutes: Routes = [
-  { path: 'events/new', component: CreateEventComponent },
+  {
+    path: 'events/new', component: CreateEventComponent,
+    canDeactivate: ['canDeactivateCreateEvent']
+  },
   { path: 'events', component: EventsListComponent },
   {
     path: 'events/:id', component: EventDetailsComponent,
