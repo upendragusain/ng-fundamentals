9cb52ab5971b55f741b58d121d651fa9735158e4
Upendra Gusain <upendra.gusain@gmail.com>
2020-04-16 11:06:37 +0100

    use ngClass to set css class

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -6,7 +6,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
   <div class="well hoverwell thumbnail">
     <h2>{{event?.name}}</h2>
     <div>Date : {{event?.date}}</div>
-    <div [ngSwitch]="event?.time">
+    <div [ngClass]="getStartTimeClass()"
+      [ngSwitch]="event?.time">
       Time : {{event?.time}}
       <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
       <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
@@ -26,6 +27,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 `
   ,
   styles: [`
+  .green {color: #003300 !important;}
+  .bold {font-weight: bold;}
   .thumbnail {min-height:210px;}
   .pad-left { margin-left: 10px;}
   .well div { color: #bbb; }
@@ -35,4 +38,9 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 export class EventThumbnailComponent {
   @Input() event: any
 
+  getStartTimeClass() {
+    if (this.event && this.event.time === '8:00 am')
+      return ['green', 'bold']
+    return []
+  }
 }
